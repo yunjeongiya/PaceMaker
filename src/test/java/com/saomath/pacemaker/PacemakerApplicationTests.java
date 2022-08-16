48c70c6117cecba1793532a6a9646a1192dd2971
yunjeong <uu0078@naver.com>
2022-08-16 13:12:49 +0900

    first commit

---
+++ src/test/java/com/saomath/pacemaker/PacemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saomath.pacemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
